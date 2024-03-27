fdbd34ce2e657a865e3c64b910c88ea6a0cc423b
Muhamad yusuf <137725546+evaseries@users.noreply.github.com>
2024-03-27 14:52:02 +0700

    change title head "Yuk Pilih"

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Yuk Pilih</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
@@ -456,7 +456,7 @@
         pollID = $(this).attr('data-poll-id');
         console.log(pollID);
     });
-        
+
         // Function to delete the poll
     function deletePoll(pollId) {
         $.ajax({
