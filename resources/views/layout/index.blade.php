1bbd969109f3d4debfa4037f58311af042cc43cd
Muhamad yusuf <137725546+evaseries@users.noreply.github.com>
2024-03-30 10:49:22 +0700

    add hiden logout if doesnt login

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -13,7 +13,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary shadow">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Yuk Pilih</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -25,30 +25,23 @@
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
                     </li>
-                    <li class="nav-item dropdown">
+                    {{-- <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
                             Dropdown
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+
                         </ul>
-                    </li>
+                    </li> --}}
                     <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
-                    <button type="button id='btnLogout" onclick="logout()" class="btn btn-light">logout</button>
+                    <button type="button" id="btnLogout" onclick="logout()" class="btn btn-light">logout</button>
                 </form>
 
                 <div class="d-block" id="btnAuth">
@@ -493,7 +486,18 @@
             $('#confirmDeletePoll').data('poll-id', pollId);
         });
     });
+    function hideLogoutButton() {
+        var token = getToken();
+        if (!token) {
+            $('#btnLogout').addClass('d-none');
+        } else {
+            $('#btnLogout').removeClass('d-none');
+        }
+    }
 
+    $(document).ready(function() {
+        hideLogoutButton();
+    });
     </script>
 </body>
 
